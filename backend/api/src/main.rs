bb855600fcc6a68b5c81646dcd2a10a50c44a8cd
kentaro_kami <kentaro_kami@kenduck.com>
2024-04-28 10:53:57 +0900

    数当て完成

--- backend/api/src/main.rs
+++ backend/api/src/main.rs
@@ -1,15 +1,33 @@
-use std::io;
+use rand::Rng;
+use std::{cmp::Ordering, io};
 
 fn main() {
     println!("Guess the number!"); // 数を当ててごらん
 
-    println!("Please input your guess."); // ほら、予想を入力してね
+    let secret_number = rand::thread_rng().gen_range(1..101);
 
-    let mut guess = String::new();
+    loop {
+        println!("Please input your guess.");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line"); // 行の読み込みに失敗しました
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("You guessed: {}", guess); // 次のように予想しました: {}
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),  //小さすぎ！
+            Ordering::Greater => println!("Too big!"), //大きすぎ！
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            } //やったね！
+        }
+    }
 }
